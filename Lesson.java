a4b18993b404db66b164c33d5281ada46ee5ee48
salmaselim77 <salmaselim7710@gmail.com>
2025-11-16 14:45:19 +0200

    Update Lesson.java

---
+++ Lesson.java
@@ -0,0 +1,26 @@
+import java.util.List;
+public class Lesson {
+    private String lessonId;          
+    private String title;             
+    private String content;           
+    private List<String> resources;
+    public String getLessonId() { 
+        return lessonId; }
+    public void setLessonId(String lessonId) { 
+        this.lessonId = lessonId; }
+
+    public String getTitle() { 
+        return title; }
+    public void setTitle(String title) { 
+        this.title = title; }
+
+    public String getContent() { 
+        return content; }
+    public void setContent(String content) { 
+        this.content = content; }
+
+    public List<String> getResources() { 
+        return resources; }
+    public void setResources(List<String> resources) { 
+        this.resources = resources; }
+}
