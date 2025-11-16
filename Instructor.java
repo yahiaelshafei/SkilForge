2717b5126572694f0cbb9a3fbdbdbe57109c489d
salmaselim77 <salmaselim7710@gmail.com>
2025-11-16 14:45:29 +0200

    Update Instructor.java

---
+++ Instructor.java
@@ -0,0 +1,16 @@
+import java.util.List;
+public class Instructor extends User {
+    private List <String> createdCourses;
+     public Instructor(String userId, String username, String email, String passwordHash,
+                      List<String> createdCourses) {
+        super(userId, "instructor", username, email, passwordHash); // role is "instructor"
+        this.createdCourses = createdCourses;
+    }
+    public List<String> getCreatedCourses() {
+        return createdCourses;
+    }
+
+    public void setCreatedCourses(List<String> createdCourses) {
+        this.createdCourses = createdCourses;
+    }   
+}
